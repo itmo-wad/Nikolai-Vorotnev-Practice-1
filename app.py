dbe12aba25b0a2f2b8cc311b9ced93e0b2156fc1
Nikolai Vorotnev <vorotnev@niuitmo.ru>
2022-03-25 18:35:21 +0300

    MongoDB connected

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+from http import client
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/practice1"
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
