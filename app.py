058b7bbf945e0469503638f5b3e8645cdc6f52f3
Nikolai Vorotnev <vorotnev@niuitmo.ru>
2022-03-25 19:42:55 +0300

    Created simple credentials check

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from http import client
+from re import L
+from flask import g, request
+from wsgiref.util import request_uri
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
@@ -10,8 +13,22 @@ mongo = PyMongo(app)
 def index():
     return render_template("index.html")
 
-@app.route('/signup')
-def contacts():
-    return render_template("signup.html")
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = mongo.db.users.find_one({})
+        if password == (user['password']):
+            return 'Success'
+        else:
+            return 'Access denied'
+
+        return 'Input: ' + username + '; ' + password
+    else:
+        # print (mongo.db.find())
+        return render_template("signup.html")
+        
 
 app.run(host="localhost", port=5000, debug=True)
