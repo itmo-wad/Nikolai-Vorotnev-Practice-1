09399be7214896ed9b0f7eaba3921bfde794aac7
Nikolai Vorotnev <vorotnev@niuitmo.ru>
2022-03-25 20:24:27 +0300

    Created sign up and sign in pages

--- app.py
+++ app.py
@@ -13,22 +13,36 @@ mongo = PyMongo(app)
 def index():
     return render_template("index.html")
 
-@app.route('/signup', methods=["GET", "POST"])
-def signup():
+@app.route('/auth', methods=["GET", "POST"])
+def auth():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
 
-        user = mongo.db.users.find_one({})
+        user = mongo.db.users.find_one({
+            "username" : username
+        })
         if password == (user['password']):
-            return 'Success'
+            return render_template("secret.html")
         else:
             return 'Access denied'
 
         return 'Input: ' + username + '; ' + password
     else:
-        # print (mongo.db.find())
+        return render_template("auth.html")
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        mongo.db.users.insert_one({
+            "username" : username, 
+            "password" : password
+        })
+        return "You was successfully registered!"
+    else:
         return render_template("signup.html")
-        
 
 app.run(host="localhost", port=5000, debug=True)
